9176459c4278aaffa3615a5c96a15c22ed5cfaf4
Iffat <iffatsattar6@gmail.com>
2025-08-20 08:40:00 -0700

    update

--- src/components/weather/temp.jsx
+++ src/components/weather/temp.jsx
@@ -8,7 +8,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=466e6fbf504ddb4fde33383e1cb2d9aa`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={Your api key}`;
 
       let res = await fetch(url);
       let data = await res.json();
